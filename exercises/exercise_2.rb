2f90b144e3f7b2836c09099294e22286b4747ed9
Konrad <konrad.kipczak@gmail.com>
2018-02-27 02:58:58 +0000

    completed exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,11 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find(1)
+
+@store2 = Store.find(2).update(name: "Winners")
+
+@store2 = Store.find(2)
+
+puts @store1.name
+puts @store2.name
