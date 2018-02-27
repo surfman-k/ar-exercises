72bc38e2b09f368ce0f957cf3f3a1fee7dfe0093
Konrad <konrad.kipczak@gmail.com>
2018-02-27 03:03:52 +0000

    fixed structure on exercise 2 and completed exercise 3

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -6,9 +6,9 @@ puts "----------"
 
 @store1 = Store.find(1)
 
-@store2 = Store.find(2).update(name: "Winners")
-
 @store2 = Store.find(2)
+
+@store2.update(name: "Winners")
 
 puts @store1.name
 puts @store2.name
